70cab3e3852f25ffb411d86361555110f8b1c557
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-09-06 09:32:51 +0800

    add: debug using vscode

--- future.js
+++ future.js
@@ -1,5 +1,5 @@
 const future = (age, gap) => {
-  return `In ${gap} years you will be  ${(age) - gap} <br>`;
+  return `In ${gap} years you will be  ${Number(age) - gap} <br>`;
 };
 
 export default future;
