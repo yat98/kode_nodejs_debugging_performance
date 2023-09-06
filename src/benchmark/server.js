9c0800caf90bbe7d9d58cdffbab7ad2fac762233
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-09-06 22:01:23 +0800

    add: benchmark mode production

--- src/benchmark/server.js
+++ src/benchmark/server.js
@@ -1,11 +1,17 @@
 import express from "express";
+import path from 'path';
 import 'dotenv/config';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const port = process.env.PORT;
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.get('/hello', (req,res) => {
-  res.send('Hello, World!');
+  res.render('hello', { title: 'Express' });
 });
 
 app.listen(port, () => {
