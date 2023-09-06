d7565d431ec6daf54e9774b6cefdfb46e73caaf7
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-09-06 09:59:14 +0800

    add: stack trace limit in prod mode

--- src/stack_trace/server.js
+++ src/stack_trace/server.js
@@ -4,6 +4,11 @@ import router from "./routes.js";
 
 const port = process.env.PORT;
 const app = express();
+
+if(process.env.NODE_ENV !== 'production'){
+  Error.stackTraceLimit = Infinity;
+}
+
 app.use(router);
 
 app.listen(port, () => {
