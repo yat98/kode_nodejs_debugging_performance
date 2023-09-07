92daa826a9cf401601b63fd7810843c4908cabb6
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-09-07 16:40:46 +0800

    add: benchmark post

--- src/benchmark/server.js
+++ src/benchmark/server.js
@@ -7,11 +7,20 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const port = process.env.PORT;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.get('/hello', (req,res) => {
   res.render('hello', { title: 'Express' });
+});
+
+app.post('/echo', (req,res) => {
+  res.send(req.body);
 });
 
 app.listen(port, () => {
