cba7a8d7f96f295ec774a209dd079e14d12dac19
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-09-08 18:53:08 +0800

    add: optimize cache and queue

--- src/async_optim/server.js
+++ src/async_optim/server.js
@@ -1,35 +1,62 @@
 import express from "express";
 import 'dotenv/config';
 import db from './db.js';
+import fastq from "fastq";
+import { LRUCache } from "lru-cache";
 
 const port = process.env.PORT;
 const app = express();
+const cache = new LRUCache({
+  max: 100,
+  ttl: 1000 * 60 * 1,
+
+});
+const queue = fastq(worker);
 const collection = await db();
+
+async function worker(cart, cb) {
+  if(cache.get(cart)) {
+    cb(null, cache.get(cart));
+  }else{
+    await collection.aggregate([
+      {$match: {cart: cart}},
+      {
+        $group: {
+          _id: "$cart", 
+          total: {
+            $sum: {
+              $multiply: ["$price", "$quantity"]
+            }
+          }
+        }
+      }
+    ]).next()
+      .then((val) => {
+        cache.set(cart, val);
+        cb(null, cache.get(cart));
+      });
+  }
+
+  
+}
 
 app.get('/carts/:id', async (req, res) => {
   const cart = parseInt(req.params.id);
+  queue.push(cart, (err,result) => {
+    if(err) {
+      res.json({error: err.message}); 
+      return;
+    }
+    res.json(result);
+    return;
+  })
 
   // const data = await collection.find({cart}).toArray();
   // if(data.length > 0) {
   //   const total = data.reduce((acc,e) => (acc += e.price * e.quantity), 0);
   //   return res.send({total});
   // }
-
-  const data = await collection.aggregate([
-    {$match: {cart: cart}},
-    {
-      $group: {
-        _id: "$cart", 
-        total: {
-          $sum: {
-            $multiply: ["$price", "$quantity"]
-          }
-        }
-      }
-    }
-  ]).toArray();
-
-  return res.send({total: data[0]});
+  // return res.send({total: data[0]});
 });
 
 app.listen(port, () => {
